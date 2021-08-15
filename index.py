8586b452ebcf21440ebd733bbc83204b374dbed1
Ajinkya Rathod <rajinkya26@gmail.com>
2021-08-15 07:43:32 +0530

    Operations on header and trailer And Naming of Custom Columns

--- index.py
+++ index.py
@@ -1,5 +1,30 @@
 import pandas as pd
 
 
-df = pd.read_csv('data/Customers_20210813185020.txt', sep="|")
+df = pd.read_csv('data/Customers_20210813185020.txt', sep="|", header=None)
+
+# header may or may not exist, so using `skiprows = 1` is not good idea
+is_header = df.iloc[0, 0]
+is_trailer = df.iloc[df.shape[0] - 1, 0]
+
+# checking if Header Records exists
+if is_header == 'H':
+    df.drop(df.head(1).index, inplace=True)
+
+# Naming the Columns
+df.columns = ["D",
+              "customerName", "customerID",
+              "customerOpenDate", "lastConsultedDate",
+              "vaccinationType", "doctorConsulted",
+              "state", "country", "postCode",
+              "dateofBirth", "activeCustomer"
+             ]
+
+# Checking if Trailer record exists
+if is_trailer == 'T':
+    df.drop(df.tail(1).index, inplace=True)
+
+# Dropping D columns as it of no use
+del df['D']
+
 print(df)
